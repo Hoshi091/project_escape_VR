a7a53435fe006db067cbec418522c623a0222b9a
Elfonator <elfo.here@gmail.com>
2025-05-08 14:57:08 +0200

    XR rig setup for simulation, logic for exit doors, keypad animation, scripts update

--- Assets/Scripts/KeypadController.cs
+++ Assets/Scripts/KeypadController.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public class KeypadController : MonoBehaviour
 {
     public string correctCode = "123";
-    public GameObject indicatorCube; // light to change color
+    public GameObject indicatorCube;
     public AudioSource successSound;
     public AudioSource failSound;
+    public Animator doorAnimator;
+    public string openTriggerName = "Open";
 
     private string currentInput = "";
+    private List<KeypadButton> pressedButtons = new List<KeypadButton>();
 
-    public void PressKey(string key)
+    public void PressKey(string key, KeypadButton button)
     {
         currentInput += key;
+        pressedButtons.Add(button);
 
         if (currentInput.Length >= correctCode.Length)
         {
-            if (currentInput == correctCode)
+            bool correct = currentInput == correctCode;
+
+            if (correct)
             {
                 Debug.Log("Correct Code!");
                 indicatorCube.GetComponent<Renderer>().material.color = Color.green;
                 successSound?.Play();
-                // TODO: unlock door or trigger event
-            }
-            else
+                doorAnimator?.SetTrigger(openTriggerName); 
             {
                 Debug.Log("Wrong Code.");
                 indicatorCube.GetComponent<Renderer>().material.color = Color.red;
                 failSound?.Play();
             }
 
-            currentInput = ""; // reset
+            StartCoroutine(ResetAfterDelay()); 
         }
+    }
+    }
+
+    private IEnumerator ResetAfterDelay() 
+    {
+        yield return new WaitForSeconds(1f);
+
+        currentInput = "";
+
+        foreach (var btn in pressedButtons)
+        {
+            btn.ResetButton();
+        }
+
+        pressedButtons.Clear();
     }
 }
